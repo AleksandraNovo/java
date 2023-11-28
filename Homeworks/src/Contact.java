013b780ff9910ac3853fc3ffa9d95a1e8ddb2746
AleksandraNovo <144597376+AleksandraNovo@users.noreply.github.com>
2023-11-28 17:07:08 +0300

    Update Contact.java

--- Homeworks/src/Contact.java
+++ Homeworks/src/Contact.java
@@ -1,16 +1,16 @@
 public record Contact(String name, String email) {
-    //private string email = email;
-    public Contact {
-        if (name == null || email == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Error");
-        }
+    public Contact(String name, String email) {
+        if (name == null || name.isEmpty()) {
+            System.out.println("Ошибка в имени пользователя и адресе почты!");
+            throw new IllegalArgumentException("Invalid arguments for Contact");
+        } else if (email == null || !isValidEmail(email)) {
+            System.out.println("Ошибка адреса электронной почты!");
+            throw new IllegalArgumentException("Invalid arguments for Contact");
+        }    
+        this.name = name;
+        this.email = email;
     }
-    //public static int giveEmail() {
-    //    return email;
-    //}
-    public Contact(String name) {
-        this(name, "non@gmail.com");
-    }
+    }    
 
     private static boolean EmailIsValid(String email) {
         if (!email.contains("@gmail.com")) {
